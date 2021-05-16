8bb5115814aac9477c237ce55756c92a5594b943
SatbirSingh28 <satbir.singh5047@gmail.com>
2021-05-16 10:51:12 -0600

    firebase changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAn_jnv7EneNBBCarjtSkqj0nuLw5v0EOk",
-  authDomain: "e-clone-e81fb.firebaseapp.com",
-  projectId: "e-clone-e81fb",
-  storageBucket: "e-clone-e81fb.appspot.com",
-  messagingSenderId: "758959420243",
-  appId: "1:758959420243:web:98430e7f8a4d153ca067a6",
-  measurementId: "G-JWE2L84PMB",
+  apiKey: "AIzaSyCaOrWRifCH8OzZAIWlwTYrroXFy-7D5Jk",
+  authDomain: "website-clone-87043.firebaseapp.com",
+  projectId: "website-clone-87043",
+  storageBucket: "website-clone-87043.appspot.com",
+  messagingSenderId: "390170218921",
+  appId: "1:390170218921:web:b40c4c74b772bd383d74b8",
+  measurementId: "G-KZCRJ0EGNH",
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
